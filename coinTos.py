5c8533dcb3c2be6af2b1c30b355fced6fe3164b1
root <root@hakumaiLaptop>
2024-07-04 12:40:23 +0900

    Victory message added

--- coinTos.py
+++ coinTos.py
@@ -21,5 +21,10 @@ def main():
 
     print(f"Heads: {heads_count}, Tails: {tails_count}")
 
+    if heads_count > tails_count:
+        print("You won!")
+    else:
+        print("You lost!")
+
 if __name__ == "__main__":
     main()
